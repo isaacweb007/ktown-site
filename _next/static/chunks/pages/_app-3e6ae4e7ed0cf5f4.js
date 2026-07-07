8e8faa46b93e8e03b35ded4aa733e799cb5fdd59
msc690428 <msc690428@gmail.com>
2026-07-07 13:57:09 +0700

    deploy: ktown.cc landing 2026-07-07_1357

---
+++ _next/static/chunks/pages/_app-3e6ae4e7ed0cf5f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4787:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4795)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4787),n(2355))),_N_E=_.O()}]);
